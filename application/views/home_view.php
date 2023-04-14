2102056543896de473eca404864390d103556255
JayC <jayc10.belmores@gmail.com>
2023-04-14 08:57:41 +0800

    Done!

---
+++ application/views/home_view.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col mt-5">
+    <h2>Kuya JayC</h2>
+<a href="<?= base_url('Home/view_products') ?>" class="btn btn-warning">Enter</a>
+
+    </div>
+</div>
+
